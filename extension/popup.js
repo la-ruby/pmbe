c0db08e30d24dc3cc8b8b675669eedb71663370c
Sam W <sam@sam-we.com>
2022-04-11 16:16:13 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -40,8 +40,6 @@ function setPageBackgroundColor() {
       elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
       elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
       document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')[0].appendChild(elemDiv)
-
-
     }
   }
   xhr.send();
