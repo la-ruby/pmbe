97ad8ef9d1ae569b23cd7e6a0bbe4cfed5d47718
Sam W <sam@sam-we.com>
2022-04-11 16:19:00 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -26,9 +26,14 @@ function setPageBackgroundColor() {
   elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
   elemDiv.innerHTML = 'VENDOR NAME XYZ APPAREL'; 
   document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')[0].appendChild(elemDiv)
+  let divs = document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')
 
   // http://postman-echo.com/get?aaa=AAA
   // https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new
+
+  for (i = 0; i < divs.length; ++i) {
+    console.log(i)
+  }
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new", true);
   xhr.onreadystatechange = function() {
