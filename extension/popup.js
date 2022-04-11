7225d5f1843ac861de32b62fc0a9591cc93dfe4e
Sam W <sam@sam-we.com>
2022-04-11 15:29:05 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -22,5 +22,5 @@ function setPageBackgroundColor() {
     document.body.style.backgroundColor = color;
   });
   alert('here')
-  alert(jQuery('.collection-list__product-tile .product-tile__image-line').length)
+  console.log(jQuery('.collection-list__product-tile .product-tile__image-line'))
 }
