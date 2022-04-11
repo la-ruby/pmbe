b1bff86998e1c57800e1f4fd3f54c0697d5d7e16
Sam W <sam@sam-we.com>
2022-04-11 16:19:52 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -33,19 +33,20 @@ function setPageBackgroundColor() {
 
   for (i = 0; i < divs.length; ++i) {
     console.log(i)
-  }
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      // JSON.parse does not evaluate the attacker's scripts.
-      //alert(xhr.responseText);
-
-      var elemDiv = document.createElement('div');
-      elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
-      elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
-      document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')[0].appendChild(elemDiv)
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        // JSON.parse does not evaluate the attacker's scripts.
+        //alert(xhr.responseText);
+    
+        var elemDiv = document.createElement('div');
+        elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
+        elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
+        document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')[0].appendChild(elemDiv)
+      }
     }
+    xhr.send();
   }
-  xhr.send();
+
 }
