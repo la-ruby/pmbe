73c659424defcfdf8a8e412e6cb42ae6aae7ac33
Sam W <sam@sam-we.com>
2022-04-11 16:20:15 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -39,11 +39,10 @@ function setPageBackgroundColor() {
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         //alert(xhr.responseText);
-    
         var elemDiv = document.createElement('div');
         elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
         elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
-        document.querySelectorAll('.collection-list__product-tile .product-tile__image-line')[0].appendChild(elemDiv)
+        divs[i].appendChild(elemDiv)
       }
     }
     xhr.send();
