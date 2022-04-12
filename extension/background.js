a2bf09cfb529c0bec066972adf903520443a7848
Sam W <sam@sam-we.com>
2022-04-12 00:23:12 -0700

    extension/background.js

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,9 @@
 let color = '#3aa757';
+let pmbe_token = undefined;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+
+  chrome.storage.sync.set({ pmbe_token });
 });
