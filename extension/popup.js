9d127580896d0aaddd4ed65770744fee771a5c4d
Sam W <sam@sam-we.com>
2022-04-12 00:45:45 -0700

    extension/popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -15,7 +15,6 @@ chrome.storage.sync.get("pmbe_token", ({ pmbe_token }) => {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeTokenButton.addEventListener("click", async () => {
-  alert('here1')
   chrome.storage.sync.set({ "pmbe_token": changeToken.value });
 });
 
