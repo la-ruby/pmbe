10ecfeb1c23b71d48105c9b072a3d0e26e10dd3c
Sam W <sam@sam-we.com>
2022-04-11 18:18:40 -0700

    extension/sj_twitter.js

--- extension/sj_twitter.js
+++ extension/sj_twitter.js
@@ -16,7 +16,6 @@ alert("D3BUG a")
         var elemDiv = document.createElement('div');
         elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
         elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
-        debugger
         item.appendChild(elemDiv)
       }
     }
