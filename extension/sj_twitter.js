8f443310266539f23474aa314eb5afc971be4175
Sam W <sam@sam-we.com>
2022-04-11 17:33:00 -0700

    extension/sj_twitter.js

--- extension/sj_twitter.js
+++ extension/sj_twitter.js
@@ -1,4 +1,4 @@
-  console.log("here2")
+console.log("aaa")
 
 
   let divs = document.querySelectorAll('.collection-list__product-tile')
@@ -11,7 +11,7 @@
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new", true);
     xhr.onreadystatechange = function() {
-      console.log("here2")
+      console.log("bbb")
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         //alert(xhr.responseText);
