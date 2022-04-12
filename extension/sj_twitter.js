6e3d7b5d43a8e65c86ea1b165a3a873a74aa899a
Sam W <sam@sam-we.com>
2022-04-11 18:19:07 -0700

    extension/sj_twitter.js

--- extension/sj_twitter.js
+++ extension/sj_twitter.js
@@ -4,8 +4,6 @@ divs.forEach(function(item) {
   xhr.open("GET", "https://pmbe-backend.herokuapp.com/experiment", true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      // JSON.parse does not evaluate the attacker's scripts.
-      //alert(xhr.responseText);
       var elemDiv = document.createElement('div');
       elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
       elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
