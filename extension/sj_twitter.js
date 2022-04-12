24d5669c8670615d772a84e7135728d6d3cc8bd1
Sam W <sam@sam-we.com>
2022-04-11 18:17:49 -0700

    extension/sj_twitter.js

--- extension/sj_twitter.js
+++ extension/sj_twitter.js
@@ -6,19 +6,17 @@ alert("D3BUG a")
   // http://postman-echo.com/get?aaa=AAA
   // https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new
 
-  alert(divs.length)
   divs.forEach(function(item) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://pmbe-backend.herokuapp.com/experiment", true);
     xhr.onreadystatechange = function() {
-      console.log("D3BUG b")
-      console.log("bbb")
       if (xhr.readyState == 4) {
         // JSON.parse does not evaluate the attacker's scripts.
         //alert(xhr.responseText);
         var elemDiv = document.createElement('div');
         elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
         elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
+        debugger
         item.appendChild(elemDiv)
       }
     }
