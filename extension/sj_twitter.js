6f6a54881f63aa060c835b540a6c491006ce5ae8
Sam W <sam@sam-we.com>
2022-04-11 18:19:32 -0700

    extension/sj_twitter.js

--- extension/sj_twitter.js
+++ extension/sj_twitter.js
@@ -1,17 +1,19 @@
-let divs = document.querySelectorAll('.collection-list__product-tile')
-divs.forEach(function(item) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://pmbe-backend.herokuapp.com/experiment", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var elemDiv = document.createElement('div');
-      elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
-      elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
-      item.appendChild(elemDiv)
+setTimeout(function() {
+  let divs = document.querySelectorAll('.collection-list__product-tile')
+  divs.forEach(function(item) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pmbe-backend.herokuapp.com/experiment", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        var elemDiv = document.createElement('div');
+        elemDiv.style.cssText = 'font-size: 50%; position:absolute;top:1px;left:1px;width:100%;height:100%;opacity:1;z-index:100;background-color:rgba(0,0,0,0.5);color:white';
+        elemDiv.innerHTML = 'VENDOR NAME: ' + xhr.responseText; 
+        item.appendChild(elemDiv)
+      }
     }
-  }
-  xhr.send();
-})
+    xhr.send();
+  })
+}, 5000)
 
 
 
